5fbd8b6c034d10cf2c984815103f5d83f6314b48
Conrad Menchine <waterflame99@gmail.com>
2019-04-30 18:51:51 -0700

    coolio

--- src/email.test.js
+++ src/email.test.js
@@ -8,3 +8,15 @@ test('banana@ucsc.edu', () => {
 test('sluggy@gmail.com', () => {
   expect(validateEmail('sluggy@gmail.com')).toBeTruthy();
 });
+
+test('scotty', () => {
+  expect(validateEmail('@scotty.thotty')).toBeFalsy();
+});
+
+test('grandma', () => {
+  expect(validateEmail('Hello, my grandson set up an email for me, is this the @google mail? please send me to the google mail thank you. Nathan. Show Nathan. Google, show me pictures of my grandson Nathan please. Hello?')).toBeTruthy();
+});
+
+test('coolzone1992@hotmail.com', () => {
+  expect(validateEmail('cool.zone1992@hotmail.com')).toBeTruthy();
+});
